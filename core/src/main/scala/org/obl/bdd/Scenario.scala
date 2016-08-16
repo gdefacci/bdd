b93097b03c085bf7368a710fb451e0df8a330eb2
gdefacci <gdefacci@yahoo.it>
2016-08-16 21:48:18 +0200

    move Scenario to core project

---
+++ core/src/main/scala/org/obl/bdd/Scenario.scala
@@ -0,0 +1,22 @@
+package org.obl.bdd
+
+sealed trait ScenarioLike[S, E]
+
+case class Scenario[S, E](title: String, assertion: Assertion[S, E]) extends ScenarioLike[S, E] {
+  override def toString = title + "\n\n" + assertion.toString()
+}
+
+case class OutlineScenario[I, S, E](title: String, table: Seq[I], f: I => Scenario[S, E]) extends ScenarioLike[S, E] {
+
+  def scenarios: Seq[Scenario[S, E]] = table.map(f)
+
+}
+
+object OutlineScenario {
+  case class Factory[I](title: String, table: Seq[I]) {
+    def apply[S, E](f: I => Scenario[S, E]) = OutlineScenario[I, S, E](title, table, f)
+  }
+
+  def apply[I](title: String, table: Seq[I]) = Factory[I](title, table)
+}
+
--- testkit/src/main/scala/org/obl/bdd/Feature.scala
+++ testkit/src/main/scala/org/obl/bdd/Feature.scala
@@ -13,24 +13,3 @@ class Feature[S, E](val title: String,
   }
 
 }
-
-sealed trait ScenarioLike[S, E]
-
-case class Scenario[S, E](title: String, assertion: Assertion[S, E]) extends ScenarioLike[S, E] {
-  override def toString = title + "\n\n" + assertion.toString()
-}
-
-case class OutlineScenario[I, S, E](title: String, table: Seq[I], f: I => Scenario[S, E]) extends ScenarioLike[S, E] {
-
-  def scenarios: Seq[Scenario[S, E]] = table.map(f)
-
-}
-
-object OutlineScenario {
-  case class Factory[I](title: String, table: Seq[I]) {
-    def apply[S, E](f: I => Scenario[S, E]) = OutlineScenario[I, S, E](title, table, f)
-  }
-
-  def apply[I](title: String, table: Seq[I]) = Factory[I](title, table)
-}
-
